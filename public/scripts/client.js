ca443ccfbb92b5cbdd221a8706d480a04e146210
Jay Chai <chunsaa@gmail.com>
2019-11-04 20:44:19 +0000

    implemented tweet display from database and created function to create the tweet element html code to append to index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,48 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+$(document).ready(function() {
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function(tweet) {
+  let $tweet = $('<article>').addClass('tweet');
+  const htmlCode = `
+  <header>
+    <img src="${tweetData.user.avatars}">         
+    <p>
+      ${tweet.user.name}
+      <span>${tweet.user.handle}</span>
+    </p>                     
+  </header>
+  <p>${tweet.content.text}</p>
+  <footer>
+    <p>${tweet.created_at}
+      <span>                
+        <i class="fa fa-flag">&#160</i>
+        <i class="fa fa-retweet">&#160</i>
+        <i class="fa fa-heart"></i>
+      </span>
+    </p>                            
+  </footer>
+  `;
+  $tweet.append(htmlCode);
+  return $tweet;
+}
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
