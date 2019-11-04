6d903911743d3bf009c778c547b55726bec27a90
Jay Chai <chunsaa@gmail.com>
2019-11-04 22:20:27 +0000

    added tweet load function from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,32 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 const createTweetElement = function(tweet) {
   let $tweet = $('<article>').addClass('tweet');
   const htmlCode = `
@@ -62,13 +36,29 @@ const renderTweets = function(tweets) {
 }
 
 $(document).ready(function() {
-  renderTweets(data);
+  const loadTweets = function() {    
+    $.ajax({
+      url: "/tweets",
+      dataType: "json",
+      type: "GET",
+      success: function(tweetDB){
+        console.log(tweetDB);
+        renderTweets(tweetDB);
+      },
+      error: function(){
+        console.log("failed");
+      }
+    });  
+  }
+  loadTweets();
+
+  //renderTweets(data);
 
   $("#submit").submit(function(event) {
     event.preventDefault();    
     console.log($(this).find("textarea").serialize());
     $.ajax({
-      url: "/tweets/",
+      url: "/tweets",
       dataType: "text",
       type: "POST",
       contentType: "application/x-www-form-urlencoded",
