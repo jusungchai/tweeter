84a3110d974472935ab8bb592be3a83af9b04dc9
Jay Chai <chunsaa@gmail.com>
2019-11-04 20:52:16 +0000

    implemented function to take an array of tweet data object to append to the index.html file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,25 +4,38 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
 $(document).ready(function() {
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const createTweetElement = function(tweet) {
   let $tweet = $('<article>').addClass('tweet');
   const htmlCode = `
   <header>
-    <img src="${tweetData.user.avatars}">         
+    <img src="${tweet.user.avatars}">         
     <p>
       ${tweet.user.name}
       <span>${tweet.user.handle}</span>
@@ -43,9 +56,11 @@ const createTweetElement = function(tweet) {
   return $tweet;
 }
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = function(tweets) {
+  for (let tweet of tweets){
+    $('#tweets-container').append(createTweetElement(tweet));
+  }  
+}
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+renderTweets(data);
 });
